929b341fe0e9d9787f4c7f7785f5cd3110fcc110
Imama Ansari <ansariimama@yahoo.com>
2026-09-04 19:33:02 +0500

    trying to solve errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 import SecTwo from "./components/Sect-two.jsx";
 import SecThree from "./components/Sect-three.jsx";
 import SectFour from "./components/Sect-four.jsx";
 import SectFive from "./components/Sect-five.jsx";
 import SectSix from "./components/Sect-six.jsx";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
